04c586123b5e29401c3e322c34a333fccc6e654a
Danvick Miller <danvickmiller@live.com>
2018-11-02 00:40:52 +0300

    improvements to work with rxjs version >= 6

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import { TranslateLoader } from "@ngx-translate/core";
 import { knownFolders } from "tns-core-modules/file-system";
+import { from, Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import { Observable } from "rxjs/Observable";
 
 export class NativeScriptLoader extends TranslateLoader {
-    prefix = "./assets/i18n/";
-    suffix = ".json";
+	public prefix = "./assets/i18n/";
+	public suffix = ".json";
 
-    constructor(prefix?: string, suffix?: string) {
-        super();
-        this.prefix = prefix;
-        this.suffix = suffix;
-    }
+	constructor(prefix?: string, suffix?: string) {
+		super();
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
 
-    getTranslation(lang: string): Observable<any> {
-        return fromPromise(knownFolders.currentApp().getFile(`${this.prefix}${lang}${this.suffix}`).readText()).pipe(
-            map((data: any) => JSON.parse(data))
-        );
-    }
+	public getTranslation(lang: string): Observable<any> {
+		return from(knownFolders.currentApp().getFile(`${this.prefix}${lang}${this.suffix}`).readText()).pipe(
+			map((data: any) => JSON.parse(data))
+		);
+	}
 }
