f494bc7af495f2fcb39ded16f8a650911b4826cb
Hannes Rüger <hannesrueger@gmx.de>
2020-09-05 12:39:23 +0200

    fixes #5

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,13 @@ import { knownFolders } from "tns-core-modules/file-system";
 import { from, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
-export class NativeScriptLoader extends TranslateLoader {
-	public prefix;
-	public suffix;
+export class NativeScriptLoader implements TranslateLoader {
+    constructor(public prefix: string = "/assets/i18n/", public suffix: string = ".json") {
+    }
 
-	constructor(prefix: string = "./assets/i18n/", suffix: string = ".json") {
-		super();
-		this.prefix = prefix;
-		this.suffix = suffix;
-	}
-
-	public getTranslation(lang: string): Observable<any> {
-		return from(knownFolders.currentApp().getFile(`${this.prefix}${lang}${this.suffix}`).readText()).pipe(
-			map((data: any) => JSON.parse(data))
-		);
-	}
+    public getTranslation(lang: string): Observable<any> {
+        return from(knownFolders.currentApp().getFile(`${this.prefix}${lang}${this.suffix}`).readText()).pipe(
+            map((data: any) => JSON.parse(data)),
+        );
+    }
 }
